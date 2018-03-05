3b893c80e17ccf9c979d2f6fd4da0a4479d736fa
JIElite <ita3051@gmail.com>
2018-03-06 00:40:56 +0800

    fix transition issue, update learning algorithm

--- agent.py
+++ agent.py
@@ -1,17 +1,16 @@
 from itertools import count
 
+import numpy
 import torch
 from torch.autograd import Variable
 from torch.distributions import Categorical
 import torch.multiprocessing as mp
 
-
-from model import Network
 from utils import init_weights, wrap_as_variable, ensure_shared_grad, Buffer
 
 
 class Worker(mp.Process):
-    def __init__(self, env, gnet, optimizer, result_queue, n_features, n_actions, wid, gamma=0.99, n_steps=8, max_steps=10000):
+    def __init__(self, env, gnet, lnet, optimizer, eps_counter, result_queue, n_features, n_actions, wid, gamma=0.99, n_steps=8, max_steps=10000):
         super(Worker, self).__init__()
         # Configuration
         self.worker_id = wid
@@ -19,14 +18,14 @@ class Worker(mp.Process):
         self.n_steps = n_steps
         self.max_steps = max_steps
         self.env = env
-        
         # Usage data structure
         self.result_queue = result_queue
+        self.eps_counter = eps_counter
         self.buffer = Buffer(size=n_steps)
         
         # network related settings
         self.global_net = gnet
-        self.local_net = Network(n_features, n_actions)
+        self.local_net = lnet
         self.optimizer = optimizer
         init_weights(self.local_net)
 
@@ -40,26 +39,27 @@ class Worker(mp.Process):
     def learn(self, next_state, done):
         # compute n steps approximate return G_t:t+n
         V_target = Variable(torch.FloatTensor([0.0])) if done else self.local_net(wrap_as_variable(next_state))[1]
+        V_target_list = []
         for experience in self.buffer.get_reversed_experience():
             V_target = experience.reward + self.gamma* V_target
-    
-        V_pi = self.local_net(wrap_as_variable(self.buffer.memory[0].state))[1]
+            V_target_list.insert(0, V_target)
 
-        # compute loss
-        td_error = V_target - V_pi
-        critic_loss = td_error.pow(2)
-        critic_loss.data.clamp_(0, 20)
+        states = numpy.stack(self.buffer.get_n_steps_data().state)
+        V_estimates = self.local_net(wrap_as_variable(states))[1]
+        V_targets = torch.stack(V_target_list)
 
-        action = self.buffer.memory[0].action
-        log_action_prob = self.local_net(wrap_as_variable(self.buffer.memory[0].state))[0][action]
-        actor_loss = - log_action_prob * td_error.detach()
-        # print(actor_loss, critic_loss)
-        total_loss = actor_loss + 0.5*critic_loss
-
+        # compute critic loss
+        td_error = V_target.detach() - V_estimates
+        critic_loss = td_error*td_error
+        # compute actor loss
+        log_action_probs = torch.stack(self.buffer.get_n_steps_data().log_action_prob)
+        actor_loss = - log_action_probs*td_error.detach()
+        total_loss = (actor_loss + critic_loss).mean()
         # reset gradient of local network
         self.optimizer.zero_grad()
         # loss backprobagation
         total_loss.backward()
+        torch.nn.utils.clip_grad_norm(self.local_net.parameters(), 3)
         # ensure to share the gradient with global net
         ensure_shared_grad(self.global_net, self.local_net)
         # update network parameters
@@ -79,16 +79,18 @@ class Worker(mp.Process):
             self.buffer.append([obs, action, log_action_prob, reward])
             if self.buffer.is_full() or done:
                 self.learn(obs_, done)
+                self.buffer.reset()
 
             # transition
-            obs_ = obs
+            obs = obs_
             if done:
-                print('work no: {} step: {}, reward: {}'.format(self.worker_id, step, eps_reward))
+                print('work no: {} eps: {}, reward: {}'.format(self.worker_id, self.eps_counter.value, eps_reward))
                 self.result_queue.put(eps_reward)
+                self.eps_counter.value += 1
                 eps_reward = 0
                 self.buffer.reset()
                 obs = self.env.reset()
-
+        
                 
 
         
