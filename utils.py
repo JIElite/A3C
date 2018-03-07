b8b260c88e24e9ec386af5be6ead4ddadd681d42
JIElite <jielite.tw@gmail.com>
2018-03-07 15:51:25 +0800

    remove useless code

--- utils.py
+++ utils.py
@@ -41,27 +41,6 @@ def ensure_shared_grad(global_net, local_net):
         g_parameter._grad = l_parameter.grad
 
 
-# class SharedAdam(torch.optim.Adam):
-#     """
-#     This code is borrowed from Morvan's pytoch-A3C:
-#     https://github.com/MorvanZhou/pytorch-A3C
-#     """
-#     def __init__(self, params, lr=1e-3, betas=(0.9, 0.9), eps=1e-8,
-#                  weight_decay=0):
-#         super(SharedAdam, self).__init__(params, lr=lr, betas=betas, eps=eps, weight_decay=weight_decay)
-#         # State initialization
-#         for group in self.param_groups:
-#             for p in group['params']:
-#                 state = self.state[p]
-#                 state['step'] = 0
-#                 state['exp_avg'] = torch.zeros_like(p.data)
-#                 state['exp_avg_sq'] = torch.zeros_like(p.data)
-
-#                 # share in memory
-#                 state['exp_avg'].share_memory_()
-#                 state['exp_avg_sq'].share_memory_()
-
-
 Transition = namedtuple('Transition', ('state', 'action', 'log_action_prob', 'reward'))
 class Buffer:
     def __init__(self, size=8):
